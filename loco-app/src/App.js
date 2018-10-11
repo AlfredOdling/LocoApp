abd6e44b5ab14b2109e7a2d4ff4ccc9f31f8f593
Alfred Ö <alfred.odling@hotmail.se>
2018-10-11 11:15:51 +0200

    first commit

---
+++ loco-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './styles/app.scss'
+import MainMenu from './MainMenu'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainMenu/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ loco-app/src/MainMenu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './styles/app.scss'
+
+class MainMenu extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hallå hallå</p>
+      </div>
+    )
+  }
+}
+
+export default MainMenu
