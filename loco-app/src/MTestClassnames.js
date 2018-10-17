834fd35a8385b6ab52571b86820a3f8923474355
Mattias Hagberg <id14mhg@cs.umu.se>
2018-10-17 15:22:17 +0200

    forgot to add testfiles. now added

---
+++ loco-app/src/MTestClassnames.js
@@ -0,0 +1,13 @@
+
+class MTestClassnames {
+  state(){
+
+  }
+
+  constructor() {
+
+  }
+}
+
+
+classNames({  'button' : true,  'button--hidden': this.props.hidden,  'button--disabled': this.props.disabled})
