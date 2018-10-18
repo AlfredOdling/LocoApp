67892b036cbe9bd0cac920fd449164971abbd9ea
Mattias Hagberg <id14mhg@cs.umu.se>
2018-10-18 10:56:34 +0200

    renamed my working files

--- loco-app/src/App.js
+++ loco-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './styles/app.scss'
 import MainMenu from './MainMenu'
+import MTest from './MTest'
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainMenu/>
+        <MTest/>
       </div>
     )
   }
