2032aacf2d5a24b0f7f4e789210d719d389f062c
Sofia Hedlund <sofia-1994@hotmail.com>
2018-10-18 13:01:27 +0200

    hej

--- loco-app/src/App.js
+++ loco-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './styles/app.scss'
 import MainMenu from './MainMenu'
+import PreviewScreen from './PreviewScreen'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MainMenu/>
+        <PreviewScreen/>
       </div>
     )
   }
