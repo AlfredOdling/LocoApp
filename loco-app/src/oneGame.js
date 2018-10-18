f44eeb9181b2b5537905d412bb1c271133b15deb
Mattias Hagberg <id14mhg@cs.umu.se>
2018-10-18 10:57:50 +0200

    added renmed files to repo

---
+++ loco-app/src/oneGame.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './oneGameStyle.scss'
+
+class oneGame extends Component{
+  state(){
+
+  }
+  render(){
+
+    return (
+      <p>
+        Hej hej
+      </p>
+    )
+  }
+}
+
+export default oneGame
