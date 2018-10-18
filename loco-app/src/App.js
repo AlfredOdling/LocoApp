4975335a6a59a5dc8f764b3bca140804395c93c2
swenee <id14mhg@cs.umu.se>
2018-10-18 13:19:24 +0200

    trying to learn syntx

--- loco-app/src/App.js
+++ loco-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import './styles/app.scss'
 import MainMenu from './MainMenu'
-import MTest from './MTest'
+import oneGame from './oneGame'
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainMenu/>
-        <MTest/>
+        <oneGame/>
       </div>
     )
   }
--- loco-app/src/oneGame.js
+++ loco-app/src/oneGame.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react'
 import './oneGameStyle.scss'
+import oneRule from './oneRule'
 
 class oneGame extends Component{
   state(){
-
+    text: '';
   }
   render(){
 
     return (
-      <p>
-        Hej hej
-      </p>
+      <p>this.text</p>
     )
   }
 }
