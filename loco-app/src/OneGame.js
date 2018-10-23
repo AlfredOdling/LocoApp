e7ff0f31c6b6fdcf7adc52e19d2b8ff812da5ba4
swenee <id14mhg@cs.umu.se>
2018-10-23 11:14:33 +0200

    printing text, trying to print text from OneRule as an object

--- loco-app/src/OneGame.js
+++ loco-app/src/OneGame.js
@@ -4,13 +4,20 @@ import OneRule from './OneRule'
 
 class OneGame extends Component {
 
+  state = {
+//    text: "En spelare säger ett påstående som börjar med ”Vem i rummet…” och räknar till 3. På tre pekar alla spelare i rummet på den spelaren påståendet passar bäst in på. Alla dricker så många pekningar de får."
+      test: "testing"
+      //rule:
+  }
+
   render(){
-    let display = this.text;
+    let rule = OneRule.constructor("InputText");
+    //let displaytext = rule.text;
 
     return (
 
       <div class = "game">
-        <p></p>
+        <p>val= {rule.text} HHH {this.state.test} </p>
       </div>
 
     )
--- loco-app/src/OneRule.js
+++ loco-app/src/OneRule.js
@@ -1,27 +1,33 @@
-/*import React, {Component} from 'react'
+import React, {Component} from 'react'
 
 class OneRule extends Component{
-  //state = {
-  //  const text = "En";
-    //spelare säger ett påstående som börjar med ”Vem i rummet…” och räknar till 3. På tre pekar alla spelare i rummet på den spelaren påståendet passar bäst in på. Alla dricker så många pekningar de får."
-  //}
-  render(){
+  state = {
+    text: "nothing"
+  }
+    constructor(props){
+      super(props)
+      this.state = {
+                    text: props//"En spelare säger ett påstående som börjar med ”Vem i rummet…” och räknar till 3. På tre pekar alla spelare i rummet på den spelaren påståendet passar bäst in på. Alla dricker så många pekningar de får."
+                    }
 
-    return (
-      //add logga i hörnet
-      // knapp
-      // ancor? scroll?
-      <p>hej</p>
-      <p>cd
-        this.text;
-      </p>
+    }
+    //add logga i hörnet
+    // knapp
+    // ancor? scroll?
+    render(){
+      let display = this.text
 
-    )
+      return (
+
+        <div class = "game">
+          <p>{display}</p>
+        </div>
+
+      )
 
   }
 
 }
 
 
-export default oneRule
-*/
+export default OneRule
