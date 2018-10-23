aebcb915e672887b36caef221721a40e20a285ab
swenee <id14mhg@cs.umu.se>
2018-10-23 09:53:45 +0200

    first commit of the day 23 okt

--- loco-app/src/App.js
+++ loco-app/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import './styles/app.scss'
 import MainMenu from './MainMenu'
-import oneGame from './oneGame'
+import OneGame from './OneGame'
 import PreviewScreen from './PreviewScreen'
 
 
 class App extends Component {
   render() {
     return (
+
       <div>
-        <PreviewScreen/>
-        <MainMenu/>
-        <oneGame/>
+        //<PreviewScreen/>
+        //<MainMenu/>
+        <OneGame/>
 
       </div>
     )
