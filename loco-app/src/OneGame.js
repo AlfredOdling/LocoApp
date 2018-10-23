eded3e5708adad7bd5de4f91525e9d5b3662511a
swenee <id14mhg@cs.umu.se>
2018-10-23 09:58:16 +0200

    changed to capital letter in onegame and one rule

---
+++ loco-app/src/OneGame.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './oneGameStyle.scss'
+import OneRule from './OneRule'
+
+class OneGame extends Component {
+
+  render(){
+    let display = this.state.text;
+
+    return (
+
+      <div class = "game">
+        <p></p>
+      </div>
+
+    )
+  }
+}
+export default OneGame
