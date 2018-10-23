1b4c5b322b01449a9a75cc7e2d6fd4dcfda350a8
swenee <id14mhg@cs.umu.se>
2018-10-23 11:58:44 +0200

    added switch statement for each game that is ready to implement

--- loco-app/src/OneRule.js
+++ loco-app/src/OneRule.js
@@ -1,33 +1,18 @@
 import React, {Component} from 'react'
 
-class OneRule extends Component{
+class OneRule extends Component {
   state = {
     text: "nothing"
   }
-    constructor(props){
-      super(props)
-      this.state = {
-                    text: props//"En spelare säger ett påstående som börjar med ”Vem i rummet…” och räknar till 3. På tre pekar alla spelare i rummet på den spelaren påståendet passar bäst in på. Alla dricker så många pekningar de får."
-                    }
-
-    }
     //add logga i hörnet
     // knapp
     // ancor? scroll?
     render(){
-/*      let display = this.text
-
-      return (
-
-        <div class = "game">
-          <p>{display}</p>
-        </div>
-*/
+      console.log(this.props)
+      return(
+        <p>{this.props.rule}</p>
       )
-
   }
-
 }
-
 
 export default OneRule
